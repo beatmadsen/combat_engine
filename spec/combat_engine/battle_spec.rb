f0f77b540e51e032ce9ad712856912a0c8f77707
Erik Madsen <beatmadsen@gmail.com>
2018-02-19 12:53:33 +0100

    EM: step 6.2: green
    Battle continues when only two teams have surviving characters

--- spec/combat_engine/battle_spec.rb
+++ spec/combat_engine/battle_spec.rb
@@ -8,17 +8,35 @@ RSpec.describe CombatEngine::Battle do
     context 'when there\'s an ongoing battle' do
       let(:remaining_hp) { 5 }
       let(:initial_participants) do
-        (%i[a b] * 3).map do |team|
+        (%i[a b c] * 3).map do |team|
           Examples::Character.new(team: team, hp: remaining_hp).combat_facade
         end
       end
-      context 'when one team\'s members have lost all hp' do
+
+      context 'when only one team\'s members have lost all hp' do
         before do
           initial_participants.each do |c|
             next unless c.team == :a
             c.damage(attribute: :hp, amount: remaining_hp)
           end
         end
+
+        let(:battle) { initial_participants.first.battle }
+        it 'should not end battle' do
+          described_class.update(1)
+          remaining_battles = initial_participants.map(&:battle).compact.uniq
+          expect(remaining_battles).to eq([battle])
+        end
+      end
+
+      context 'when all teams but one have lost all hp' do
+        before do
+          initial_participants.each do |c|
+            next if c.team == :c
+            c.damage(attribute: :hp, amount: remaining_hp)
+          end
+        end
+
         it 'should end battle' do
           described_class.update(1)
           remaining_battles = initial_participants.map(&:battle).compact
@@ -30,8 +48,9 @@ RSpec.describe CombatEngine::Battle do
         before do
           team_a = initial_participants.select { |c| c.team == :a }
           team_b = initial_participants.select { |c| c.team == :b }
+          team_c = initial_participants.select { |c| c.team == :b }
           # Leave one member on each team with hp
-          (team_a[1..-1] + team_b[1..-1]).each do |c|
+          (team_a[1..-1] + team_b[1..-1] + team_c[1..-1]).each do |c|
             c.damage(attribute: :hp, amount: remaining_hp)
           end
         end
