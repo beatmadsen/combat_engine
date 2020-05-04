564e3e47fa90ec65106a6fda0755b32fc168e54b
Erik Madsen <beatmadsen@gmail.com>
2020-05-04 22:50:58 +0200

    wip

--- lib/combat_engine/character.rb
+++ lib/combat_engine/character.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 module CombatEngine
+  class Error < StandardError
+  end
+
+  class ImplementationError < Error
+    def initialize(msg="Method must be implemented")
+      super
+    end
+  end
+
   module Character
-    attr_accessor :hp, :effects_controller
+    attr_accessor :hp
+
+    def effects_controller
+      raise ImplementationError
+    end
+
+    def damage_controller
+      raise ImplementationError
+    end
+
+    def healing_controller
+      raise ImplementationError
+    end
+
+    def controllers
+      @_controllers ||= %i[effects damage healing].map { |prefix| [prefix, send("#{prefix}_controller") }.to_h.freeze
+    end
 
     def perform(action_class, target)
       action = action_class.new(effects_controller, target.effects_controller)
